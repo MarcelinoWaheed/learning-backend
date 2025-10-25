8067a9e34e11c1f8ce4abd0a3c274d642106b3ef
Marcelino Waheed <mo.maro87644@gmail.com>
2025-10-25 17:58:56 +0300

    Add files via upload

---
+++ models/task.model.js
@@ -0,0 +1,7 @@
+let tasks = [
+  { id: 1, title: "buy milk", completed: true },
+  { id: 2, title: "buy eggs", completed: false },
+  { id: 3, title: "take the breakfast", completed: false },
+];
+
+export { tasks };
