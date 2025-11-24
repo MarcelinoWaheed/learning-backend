1534a8813d66b18899b01fa47077226114e3a0a3
Marcelino Waheed <mo.maro87644@gmail.com>
2025-11-24 23:07:15 +0200

    main.js

--- main.js
+++ main.js
@@ -2,16 +2,26 @@ import express from "express";
 import taskRouter from "./routes/taskRoute.js";
 import connectMongo from "./config/connectMongo.js";
 import "dotenv/config";
-import TextStatus from "./utils/httpStatus.js";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 connectMongo();
+app.use(cors());
 
 app.use(express.json({ limit: "10kb" }));
 
 app.use("/api/tasks", taskRouter);
+
+app.use((err, req, res, next) => {
+	return res.status(err.statusCode || 404).json({
+		success: err.isSuccess,
+		message: err.message,
+		code: err.statusCode || 500,
+		data: null,
+	});
+});
 
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
