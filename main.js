f9ae40896c0ea166a0cf59e39db6bd2b52a1123a
Marcelino Waheed <mo.maro87644@gmail.com>
2025-12-02 23:44:47 +0200

    Add userRouter to main application

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import express from "express";
 import taskRouter from "./routes/taskRoute.js";
 import connectMongo from "./config/connectMongo.js";
+import userRouter from "./routes/userRoute.js";
 import "dotenv/config";
 import cors from "cors";
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json({ limit: "10kb" }));
 
 app.use("/api/tasks", taskRouter);
+app.use("/api/users", userRouter);
 
 app.use((err, req, res, next) => {
 	return res.status(err.statusCode || 404).json({
