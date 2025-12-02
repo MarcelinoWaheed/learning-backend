22001e118dbb4ec1a1124df0f4feb2fba5fdca5c
Marcelino Waheed <mo.maro87644@gmail.com>
2025-12-02 23:44:17 +0200

    Add files via upload

---
+++ utils/generateToken.js
@@ -0,0 +1,6 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (payload) => {
+    return jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: "30d" });
+};
+export default generateToken;
