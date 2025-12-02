66159f43deaef28fa20a74615e3cdf1c565cf1dd
Marcelino Waheed <mo.maro87644@gmail.com>
2025-12-02 23:38:43 +0200

    Fix typo in mongoose import and connection URL

--- config/connectMongo.js
+++ config/connectMongo.js
@@ -1,14 +1,14 @@
-import mongoos from "mongoose";
+import mongoose from "mongoose";
 
 const connectMongo = async () => {
     try {
         mongoose.connection.on("connected", () => {
             console.log("Database Connected Successfully");
         });
-        await mongoos.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGODB_URI);
     } catch (error) {
-        console.log(error.message);        
+        console.log(error.message);
     }
-} 
+}
 
 export default connectMongo;
